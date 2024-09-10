03eb56a900f8d6a2825b9a6deca0aae51788aaff
janicescott33 <janicescott33@icloud.com>
2024-09-10 18:02:23 +0100

    JS and moment.js libraries added

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
+function updateTime() {
+  let londonDate = document.querySelector("#london-date");
+  let londonTime = document.querySelector("#london-time");
+  londonDate.innerHTML = moment().tz("Europe/London").format("D MMMM YYYY");
+  londonTime.innerHTML = moment()
+    .tz("Europe/London")
+    .format("h:mm:ss [<small>]A[<small>]");
 
+  let parisDate = document.querySelector("#paris-date");
+  let parisTime = document.querySelector("#paris-time");
+  parisDate.innerHTML = moment().tz("Europe/Paris").format("D MMMM YYYY");
+  parisTime.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("h:mm:ss [<small>]A[<small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
